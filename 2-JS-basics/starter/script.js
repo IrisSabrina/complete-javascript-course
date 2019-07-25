aaa2068f41170f08462c5eea23aa8850857d465c
ccosat1isd <ccosat1isd@ccosat08isd.local>
2019-07-24 19:11:37 -0600

    Began truthy and falsy Values

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -196,54 +196,70 @@
 /////////////////////
 // Ternary Operators and Switch Statements vid # 16
 /////////////////////
-// A ternary operator has three parts called operands
-// it is an alternate way of writing an if statement
-let firstName = 'John';
-let age = 16;
-
-// ternary operator
-age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.'); // the condition is first, the if block (after the ?) and the then which comes after the :
-
-let drink = age >= 18 ? 'beer' : 'juice';
-console.log(drink);
-
-// if we wrote the above as an if statement it would look like this:
-
-// if (age >= 18) {
-//   let drink = 'beer';
-// } else {
-//   let drink = 'juice';
+// // A ternary operator has three parts called operands
+// // it is an alternate way of writing an if statement
+// let firstName = 'John';
+// let age = 16;
+//
+// // ternary operator
+// age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.'); // the condition is first, the if block (after the ?) and the then which comes after the :
+//
+// let drink = age >= 18 ? 'beer' : 'juice';
+// console.log(drink);
+//
+// // if we wrote the above as an if statement it would look like this:
+//
+// // if (age >= 18) {
+// //   let drink = 'beer';
+// // } else {
+// //   let drink = 'juice';
+// // }
+//
+// // Switch Statement
+// let job = 'instructor';
+//
+// switch (job) {
+//   case 'teacher':
+//   case 'instructor':
+//     console.log(firstName + ' teaches kids how to code.');
+//     break; // this stops it from evaluating other cases if the job is true
+//   case 'driver':
+//     console.log(firstName + ' drives an uber in Lisbon.');
+//     break;
+//   case 'designer':
+//     console.log(firstname + ' designs beautiful websites.');
+//     break;
+//   default:
+//     console.log(firstName + ' does something else.'); // no break is needed because it is the end of the switch
+// }
+//
+// age = 56;
+// switch (true) {
+//   case age < 13:
+//     console.log(firstName + ' is a boy.');
+//     break;
+//   case age >= 13 && age < 20:
+//     console.log(firstName + ' is a teenager.');
+//     break;
+//   case age >= 20 && age < 30:
+//     console.log(firstName + ' is a young man.');
+//     break;
+//   default:
+//     console.log(firstName + ' is a man.');
 // }
 
-// Switch Statement
-let job = 'instructor';
+/////////////////////
+// Truthy and Falsy values & equality operators vid # 17
+/////////////////////
 
-switch (job) {
-  case 'teacher':
-  case 'instructor':
-    console.log(firstName + ' teaches kids how to code.');
-    break; // this stops it from evaluating other cases if the job is true
-  case 'driver':
-    console.log(firstName + ' drives an uber in Lisbon.');
-    break;
-  case 'designer':
-    console.log(firstname + ' designs beautiful websites.');
-    break;
-  default:
-    console.log(firstName + ' does something else.'); // no break is needed because it is the end of the switch
-}
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy VALUES
+let height;
 
-age = 56;
-switch (true) {
-  case age < 13:
-    console.log(firstName + ' is a boy.');
-    break;
-  case age >= 13 && age < 20:
-    console.log(firstName + ' is a teenager.');
-    break;
-  case age >= 20 && age < 30:
-    console.log(firstName + ' is a young man.');
-    break;
-  default:
-    console.log(firstName + ' is a man.');
+height = 0;
+
+if (height || height === 0) {
+  console.log('Variable is defined');
+} else {
+  console.log('Variable has NOT been defined');
 }
